8963155d82521132e65671558e1526b5913f988c
Sönke Hahn <soenkehahn@gmail.com>
2013-04-22 00:10:00 +0200

    added some tests

---
+++ tests/04.hs
@@ -0,0 +1,28 @@
+{-# language NoImplicitPrelude #-}
+
+module F (main) where
+
+
+-- ~ import GHC.Num
+-- ~ 
+-- ~ 
+-- ~ main :: Integer
+-- ~ main = fac 6 -- Succ (Succ (Succ Zero)) * Succ (Succ (Succ (Succ Zero)))
+-- ~ 
+-- ~ fac 1 = 1
+-- ~ fac n = n * fac (n - 1)
+
+
+main :: Bool
+main = isOdd (S (S (S Z)))
+
+isOdd :: Nat -> Bool
+isOdd Z = False
+isOdd (S a) = not (isOdd a)
+
+not False = True
+not True = False
+
+
+data Nat = Z | S Nat
+data Bool = True | False
---
+++ tests/05.hs
@@ -0,0 +1,25 @@
+{-# language NoImplicitPrelude #-}
+
+module F (main) where
+
+
+-- ~ import GHC.Num
+-- ~ 
+-- ~ 
+-- ~ main :: Integer
+-- ~ main = fac 6 -- Succ (Succ (Succ Zero)) * Succ (Succ (Succ (Succ Zero)))
+-- ~ 
+-- ~ fac 1 = 1
+-- ~ fac n = n * fac (n - 1)
+
+
+main :: Nat
+main = S (S (S (S Z))) * S (S (S Z))
+
+data Nat = Z | S Nat
+
+Z + b = b
+S a + b = a + S b
+
+Z * b = Z
+S a * b = b + (a * b)

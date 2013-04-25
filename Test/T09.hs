0f74c76d1943f86b39e03f746949f748ede2da03
Sönke Hahn <soenkehahn@gmail.com>
2013-04-25 23:37:20 +0200

    fixed test files

--- Test/T09.hs
+++ Test/T09.hs
@@ -2,7 +2,13 @@
 
 module Test.T09 (main) where
 
+main :: Nat
+main = 15
 
-main :: [Int]
-main = map (+ 3) (take 10 [0 .. 50])
+data Nat = Zero | Succ Nat
+  deriving Show
 
+instance Num Nat where
+    fromInteger x | x < 0 = error "neg Nat"
+    fromInteger 0 = Zero
+    fromInteger n = Succ $ fromInteger $ pred n
